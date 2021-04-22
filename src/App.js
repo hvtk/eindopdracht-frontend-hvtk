a0cc9f381d6e6bb74f683a3ef1dc5be503ca0396
hvtk <hvtk1968@gmail.com>
2021-04-22 16:56:35 +0200

    created the react-project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Dit zijn de eerste tekens van mijn applicatie voor de eindopdracht onderdeel: frontend!
+    </div>
+  );
+}
+
+export default App;
