9f04665540a230ef2e48c2c9add22d75b18beeb8
hvtk <hvtk1968@gmail.com>
2021-04-23 12:42:55 +0200

    add gebruikersnaam in form signin

--- src/pages/signIn/SignIn.js
+++ src/pages/signIn/SignIn.js
@@ -1,16 +1,40 @@
 import React from "react";
 
+import {useForm} from "react-hook-form";
+
 //import '/SignIn.css';
 
 
 function SignIn() {
+    const {handleSubmit, register}= useForm();
+
+
+    function onFormSubmit(data) {
+      console.log(data);
+    }
 
     return (
+        <>
         <div>
             Dit is het inlogscherm!
         </div>
-    )
 
+        <form onSubmit={handleSubmit(onFormSubmit)}>
+            <h4> gebruikersnaam </h4>
+
+            <label htmlFor="gebruikersnaam">
+                <input
+                type="text"
+                name="gebruikersnaam"
+                id="gebruikersnaam"
+                placeholder="gebruikersnaam"
+                {...register("gebruikersnaam")}
+                />
+            </label>
+
+        </form>
+    </>
+    )
 }
 
 export default SignIn;
