33d9bd1450c519faeff525645b1edbf5d85c77ed
hvtk <hvtk1968@gmail.com>
2021-04-27 18:45:23 +0200

    added code for image in homepage, but it does not works

---
+++ src/components/pageBasics/PhoneNumbers.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function PhoneNumbers({image, title, description}) {
+    return (
+        <article className="phoneNumbers">
+            <img src={image} alt="to call" />
+            <h4> className="phoneNumbers-number">
+                {title}
+            </h4>
+            <h5 className="phoneNumbers-description">
+                {description}
+            </h5>
+        </article>
+    )
+}
+
+export default PhoneNumbers;
--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,22 +1,25 @@
 import React from "react";
 
 import './Homepage.css';
-
+import { ReactComponent as call} from "./assets/icons/telephoneicon.svg";
+import PhoneNumbers from "../../components/pageBasics/PhoneNumbers";
 
 
 function Homepage() {
 
     return (
-        <div >
-          <p> Dit is de startpagina! </p>
-            <h4> 112 </h4>
-        </div>
-
-
-
-
+        <>
+             <div >
+                <p> Dit is de startpagina! </p>
+                <call />
+                <PhoneNumbers
+                    image={call}
+                    title="112"
+                    description="bij levensbedrijgende situaties"
+                    />
+             </div>
+        </>
     )
-
 }
 
 
