127537793ea008981ff0b57ca9ac820784428f8f
hvtk <hvtk1968@gmail.com>
2021-04-29 15:53:09 +0200

    added first grid code for homepage. Yeaaaah

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -2,11 +2,24 @@ import React from "react";
 
 import './Homepage.css';
 //import PhoneNumbers from "../../components/pageBasics/PhoneNumbers";
-import phoneIcon from '../../assets/icons/callicon.svg'
+//import phoneIcon from '../../assets/icons/callicon.svg'
 
 function Homepage() {
 
     return (
+
+        <div className="homepageWrapper">
+            <div className="readout" id="border-top"> voorlezen </div>
+            <div className="chosen-language" id="border-right"> taalkeuze </div>
+            <div id="border-bottom"></div>
+            <div id="border-left"></div>
+            <div className="pictureHomepage" id="picture-homepage"></div>
+            <div className="phoneNumbers" id="phone-numbers"></div>
+        </div>
+    )
+}
+
+/*    return (
       <>
         <section className="containerHomepage">
           <form className="containerNumbers">
@@ -22,7 +35,7 @@ function Homepage() {
         </section>
       </>
     )
-}
+}*/
 
 
 /*return (
