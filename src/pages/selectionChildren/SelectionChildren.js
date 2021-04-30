842b5ef452367e1919214d96b7e07d945f264fc5
hvtk <hvtk1968@gmail.com>
2021-04-30 10:35:06 +0200

    added code layout selectionchildren

--- src/pages/selectionChildren/SelectionChildren.js
+++ src/pages/selectionChildren/SelectionChildren.js
@@ -1,17 +1,49 @@
 import React from "react";
+import {useForm} from "react-hook-form";
 
-//import '/SelectionChildren.css';
+import './SelectionChildren.css';
 
 
 function SelectionChildren() {
 
+
+    const {handleSubmit}= useForm();
+
+    function onFormSubmitReadout(data) {
+        console.log(data);
+    }
+
     return (
+
+        <div className="childrenLayout">
+            <div className="layoutInputBorderTop" id="border-top" onSubmit={handleSubmit(onFormSubmitReadout)}>
+                <button
+                    type="submit" >
+                    Voorlezen
+                </button>
+                <button
+                    type="submit" >
+                    Taalkeuze
+                </button>
+            </div>
+            <div id="border-right"></div>
+            <div className="layoutInputBorderBottom" id="border-bottom">
+                <h4> GEEF NIET OP! JE BENT AL ZOVER GEKOMEN. </h4>
+            </div>
+            <div id="border-left"></div>
+            <div className="phoneNumbers" id="phone-numbers"></div>
+            <div id="picture-children-background"></div>
+        </div>
+    )
+}
+
+/*    return (
         <div>
             Dit is het scherm na selectie op leeftijd voor kinderen!
         </div>
     )
 
-}
+}*/
 
 export default SelectionChildren;
 
