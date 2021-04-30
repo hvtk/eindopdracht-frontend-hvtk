17cb79aaf34592dd57f92b7a85f38167a3b2d0b6
hvtk <hvtk1968@gmail.com>
2021-04-30 13:02:55 +0200

    added code aftersearchquery with only search-problems-left box

--- src/pages/afterSearchQuery/AfterSearchQuery.js
+++ src/pages/afterSearchQuery/AfterSearchQuery.js
@@ -1,17 +1,46 @@
 import React from "react";
+import {useForm} from "react-hook-form";
 
-//import '/AfterSearchQuery.css';
+import './AfterSearchQuery.css';
 
 
 function AfterSearchQuery() {
 
+
+    const {handleSubmit}= useForm();
+
+    function onFormSubmitReadout(data) {
+        console.log(data);
+    }
+
     return (
-        <div>
-        Dit is het scherm na een keuze gemaakt te hebben voor informatie!
+
+        <div className="afterSearchLayout">
+            <div className="layoutInputBorderTop" id="border-top" onSubmit={handleSubmit(onFormSubmitReadout)}>
+                <button
+                    type="submit" >
+                    Voorlezen
+                </button>
+                <button
+                    type="submit" >
+                    Taalkeuze
+                </button>
+            </div>
+            <div id="border-right"></div>
+            <div className="layoutInputBorderBottom" id="border-bottom">
+                <h4> GEEF NIET OP! JE BENT AL ZOVER GEKOMEN. </h4>
+            </div>
+            <div id="border-left"></div>
+            <div className="searchProblemsLeft" id="search-problems-left"></div>
+
+
+
         </div>
     )
-
 }
+
+
+
 
 export default AfterSearchQuery;
 
