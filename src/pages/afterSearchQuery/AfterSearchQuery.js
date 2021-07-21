73cfebea44782aeb1fa8764bbc072fb93fd54e86
hvtk <hvtk1968@gmail.com>
2021-07-21 09:43:21 +0200

    changed ClassName camelCase and delete comments aftersearchpage

--- src/pages/afterSearchQuery/AfterSearchQuery.js
+++ src/pages/afterSearchQuery/AfterSearchQuery.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import {useForm} from "react-hook-form";
-import {Link, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import axios from "axios";
 
 import './AfterSearchQuery.css';
@@ -8,16 +8,12 @@ import './AfterSearchQuery.css';
 import Button from "../../components/buttons/Button";
 import ButtonB from "../../components/buttons/ButtonB";
 import BorderBottom from "../../components/bordersPageText/BorderBottom";
-
-import imageSearchPostalcodeAge from '../../assets/images/afterSearchQuery/ImageSeachPostalcodeAge.svg';
 import BackToHome from "../../components/BackToHome";
 import TheSelectedPage from "../../components/TheSelectedPage";
 import GoToProfilePage from "../../components/GoToProfilePage";
 import LogOut from "../../components/LogOut";
 
-
-
-
+import imageSearchPostalcodeAge from '../../assets/images/afterSearchQuery/ImageSeachPostalcodeAge.svg';
 
 function AfterSearchQuery() {
 
@@ -49,8 +45,8 @@ function AfterSearchQuery() {
 
     return (
 
-        <div className="afterSearchLayout">
-            <div className="layoutInputBorderTop" id="border-top" onSubmit={handleSubmit(onFormSubmitReadout)}>
+        <div className="after-search-layout">
+            <div className="layout-input-border-top" id="border-top" onSubmit={handleSubmit(onFormSubmitReadout)}>
                 <ButtonB>
                     Voorlezen
                 </ButtonB>
@@ -59,12 +55,9 @@ function AfterSearchQuery() {
                 </ButtonB>
             </div>
             <div id="border-right"></div>
-            {/*<div className="layoutInputBorderBottom" id="border-bottom">
-                <h4> GEEF NIET OP! JE BENT AL ZOVER GEKOMEN. </h4>
-            </div>*/}
             <BorderBottom></BorderBottom>
             <div id="border-left"></div>
-            <div className="searchProblemsLeftElderly" id="search-problems-left" onSubmit={handleSubmit(onFormSubmit)}>
+            <div className="search-problems-left-elderly" id="search-problems-left" onSubmit={handleSubmit(onFormSubmit)}>
               <p>
                 <Button>
                     Zoeken
@@ -97,9 +90,9 @@ function AfterSearchQuery() {
                 </Button>
               </p>
             </div>
-            <div className="webAddressBox" id="web-address-box">
+            <div className="web-address-box" id="web-address-box-grid">
             </div>
-            <div className="searchOptionWebQuery" id="search-option-web-query">
+            <div className="search-option-web-query" id="search-option-web-query-grid">
                 {data.map((aidWorker) => {
                     return (
                         <div>
@@ -107,7 +100,7 @@ function AfterSearchQuery() {
                         </div>)
                 })}
             </div>
-            <div className="postalCodeBox" id="postal-code-box">
+            <div className="postal-code-box" id="postal-code-box-grid">
                 <label htmlFor="postalCode-field">
                      <input
                         type="text"
@@ -118,7 +111,7 @@ function AfterSearchQuery() {
                     />
                 </label>
             </div>
-            <div className="ageBox" id="age-box">
+            <div className="age-box" id="age-box-grid">
                 <label htmlFor="ageBox-field">
                     <input
                         type="text"
@@ -129,11 +122,7 @@ function AfterSearchQuery() {
                     />
                 </label>
             </div>
-            <div className="pictureSearchWebSmall" id="picture-search-web-small"><img className="imageSearchPostalcodeAge" src={imageSearchPostalcodeAge} alt=""/> </div>
-            {/* } <div id="back-to-home" className="backToHome"> <Link to="/"> Home </Link> </div>
-            <div id="selected-page" className="selectedPage"> ZOEK-PAGINA </div>
-            <div id="go-to-profile-page" className="goToProfilePage"> <Link to="/profile"> Ga naar de profielpagina </Link> </div>
-            <div id="log-out" className="logOut"> Uit loggen </div> */}
+            <div className="picture-search-web-small" id="picture-search-web-small-grid"><img className="image-search-postalcode-age" src={imageSearchPostalcodeAge} alt=""/> </div>
             <BackToHome></BackToHome>
             <TheSelectedPage>
                 ZOEK-PAGINA
